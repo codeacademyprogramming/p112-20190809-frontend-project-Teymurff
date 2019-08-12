a407c2a7479f349fad3b211cdf9967607e8a3e7e
Teymur <you@example.com>
2019-08-13 02:16:34 +0400

    sections aboutus,acheivements and webdesigndev added

--- FRONTEND TEMPLATE EDULEARN/scripts/main.js
+++ FRONTEND TEMPLATE EDULEARN/scripts/main.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
     $(".closesidenav").click(function () {
         $("#sidenavigation").width("0");
-    });
+    }); 
 });
 
 let acc = document.getElementsByClassName("accordion-edulearn");
@@ -21,6 +21,25 @@ for (i = 0; i < acc.length; i++) {
       panel.style.maxHeight = panel.scrollHeight + "px";
     } 
   });
+
+  
+}
+
+let accabout = document.getElementsByClassName("accordion-aboutus");
+let j;
+
+for (j = 0; j < accabout.length; j++) {
+  accabout[j].addEventListener("click", function() {
+    this.classList.toggle("activeab");
+    let panelabout = this.nextElementSibling;
+    if (panelabout.style.maxHeight){
+      panelabout.style.maxHeight = null;
+    } else {
+      panelabout.style.maxHeight = panelabout.scrollHeight + "px";
+    } 
+  });
+
+  
 }
 
 
